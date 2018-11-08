ca019b527d11ce3337996ee5be3ef8757cada5fd
Sam Umirdinov <sam.umirdinov@gmail.com>
2018-11-07 16:24:29 -0800

    form for new reviews

--- src/js/restaurant_info.js
+++ src/js/restaurant_info.js
@@ -1,6 +1,8 @@
 import DBHelper from './dbhelper';
 import './sw-register.js';
 import favoriteButton from './favorite-button';
+import reviewForm from './review-form';
+
 
 
 let restaurant;
@@ -114,13 +116,19 @@ const fillReviewsHTML = (reviews = self.restaurant.reviews) => {
     const noReviews = document.createElement('p');
     noReviews.innerHTML = 'No reviews yet!';
     container.appendChild(noReviews);
-    return;
+  } else {
+    const ul = document.getElementById('reviews-list');
+    reviews.forEach(review => {
+      ul.appendChild(createReviewHTML(review));
+    });
+    container.appendChild(ul);
   }
-  const ul = document.getElementById('reviews-list');
-  reviews.forEach(review => {
-    ul.appendChild(createReviewHTML(review));
-  });
-  container.appendChild(ul);
+
+  const h3 = document.createElement('h3');
+  h3.innerHTML = "Leave a Review";
+  container.appendChild(h3);
+  const id = getParameterByName('id');
+  container.appendChild(reviewForm(id));
 }
 
 /**
